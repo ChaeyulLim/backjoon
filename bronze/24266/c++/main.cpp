128003ff46ae0875e5a143763e84d38a27ce4850
chaeyul <chaeyul0703@gmail.com>
2026-01-27 10:00:55 +0900

    24266 문제 해결

---
+++ bronze/24266/c++/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    long long n;
+    std::cin >> n;
+    unsigned long long result = n * n * n;
+    std::cout << result << '\n' << 3;
+    return 0;
+}
---
+++ bronze/24266/python/main.py
@@ -0,0 +1,6 @@
+
+if (__name__ == "__main__"):
+    n = int(input())
+    result = n * n * n
+    print(result)
+    print(3)
