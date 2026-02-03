5571cd370222065b62b8cd634324d04bb8215280
chaeyul <chaeyul0703@gmail.com>
2026-02-03 12:20:28 +0900

    2751 python 수정

--- silver/2751/python/main.py
+++ silver/2751/python/main.py
@@ -1,8 +1,9 @@
+import sys
+input = sys.stdin.readline
 
 if (__name__ == "__main__"):
     n = int(input())
     array = list(int(input()) for _ in range(n))
 
     array.sort()
-    for data in array:
-        print(data)
+    print('\n'.join(map(str, array)))
