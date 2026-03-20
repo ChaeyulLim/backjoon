d12da9f0a5b3942e15124d1c39290f809fbc816e
chaeyul <chaeyul0703@gmail.com>
2026-03-20 23:43:51 +0900

    27433 문제 해결

---
+++ silver/1874/c++/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+int main()
+{
+    int num, data;
+    std::stack<int> _stack;
+    std::vector<int> arr;    
+    for (int i = 0; i < num; i++) {
+        std::cin >> data;
+        for ()
+    }
+
+
+
+
+    return 0;
+}
