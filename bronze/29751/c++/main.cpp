51578d66df6fc331004e15b12061dccdfd1026fe
chaeyul <chaeyul0703@gmail.com>
2026-03-25 20:46:02 +0900

    29751 문제 해결

---
+++ bronze/29751/c++/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    int w, h;
+    std::cin >> w >> h;
+
+    printf("%.1f", 1.0*w*h / 2);
+    return 0;
+}
---
+++ bronze/29751/python/main.py
@@ -0,0 +1,5 @@
+
+if (__name__ == "__main__"):
+    w, h = map(int, input().split())
+
+    print(w * h / 2)
